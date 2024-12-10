0dcc4d3e8fe4480ce331322da48e784cfb387d85
Nicholas-Reffner <nick.reffner@gmail.com>
2024-12-09 20:10:18 -0600

    Added a shooting mechanice and asteroid splitting. Need to add a score system that records Highscores.

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
+import sys
 import pygame
 from constants import *
-from player import *
-from circleshape import *
-from asteroid import *
-from asteroidfield import *
-
-
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
-
-    pygame.init() 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-   
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
 
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
 
@@ -34,26 +32,30 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        for thing in updatable:
-            thing.update(dt)
 
-        for object in asteroids:
-           if object.collisions(player):
-               print("Game Over!")
-               event.type == pygame.QUIT
-               return
+        for obj in updatable:
+            obj.update(dt)
 
-        screen.fill('black')
-        
-        for thing in drawable:
-            thing.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
-        
 if __name__ == "__main__":
     main()
